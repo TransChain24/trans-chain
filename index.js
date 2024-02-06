6c10ece5268e1ccc914e25e1e86295a7a8b4e1b0
unknown <harshilmalaviya0007@gmail.com>
2024-02-06 03:57:32 -0800

    user registration half

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const common = require("./api/common");
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.use("/common", common);
+
+mongoose.connect("mongodb+srv://transchain24:transchain24@transchain-cluster.vyuaug6.mongodb.net/TransChain").then(() => {
+    app.listen(3000, () => {
+        console.log("connection on PORT 3000");
+        console.log("Mongo DB connect");
+    });
 });
