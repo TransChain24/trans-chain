8d398cd8f6233176f44077f18662686ed83fd7ae
Aryan49SM <aryansm832@gmail.com>
2024-03-12 20:03:25 +0530

    remove unneccessory files

--- tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-for(int i=0; i<oddLen; i++){
-    //     printf(" odd number: %d\n", odd[i]);
-    // }
-
-    // for(int i=0; i<evenLen; i++){
-    //     printf("even number: %d\n", even[i]);
-    // }
