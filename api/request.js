136230de1954bc665068d673ab9c90b7498a66a7
harshil <115367352+007harshil@users.noreply.github.com>
2024-04-16 09:34:04 +0530

    order request api complete

--- api/request.js
+++ api/request.js
@@ -1,6 +1,21 @@
 const { Router } = require('express');
 const request = Router();
+const requestModel = require("./../models/request");
 
+request.post("/request", async (req, res) => {
+    try {
+        const data = new requestModel(req.body);
+        const save = await data.save();
 
+        if (save) {
+            res.send({ "status": true, data: data });
+        } else {
+            res.send({ "status": false });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: false, error: "Internal Server Error" });
+    }
+});
 
 module.exports = request;
